7133d2384898230c326677535b95243de2a7465f
Hamish Paterson <hamish.paterson@crystalbrookcollection.com>
2026-09-01 14:42:22 +1000

    Initial Fourth Down app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+export default function App() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      background: "#07101f",
+      color: "white",
+      padding: "40px"
+    }}>
+      <h1>Fourth Down</h1>
+      <p>If you can see this, the custom app is running.</p>
+    </div>
+  );
+}
+``
